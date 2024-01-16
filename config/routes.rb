d2d6c02f44792dae2a137f22c5f2dd91cfada6c5
Marko Matotan <marko.matotan@kodius.com>
2024-01-16 14:15:53 +0100

    healthcheck endpoint

--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,7 @@
 Rails.application.routes.draw do
   mount Rswag::Ui::Engine => '/spec'
   mount Rswag::Api::Engine => '/spec'
+  get '/healthcheck', to: proc { [200, {}, ['success']] }
   namespace :api, format: :json do
     post 'sessions/sign-up', to: 'registrations#create'
     post 'sessions/login', to: 'sessions#create'
